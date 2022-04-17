c0b9dec904c63c56b45656d816a853f74d5f2c81
jinchenxi <1371548943@qq.com>
2022-04-17 18:33:37 +0800

    first commit

---
+++ src/main/java/com/sunny/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.sunny.git;
+
+/**
+ * @author jinchenxi
+ * @date 2022-04-17 18:24
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
